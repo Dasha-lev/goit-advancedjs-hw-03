b563cb56ecc9fcbf13173daaa815b3d0faeca688
Daria <darina.levchuk@gmail.com>
2025-03-28 14:06:51 +0200

    change to vite

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig(({ command }) => {
               return 'vendor';
             }
           },
+      
           entryFileNames: chunkInfo => {
             if (chunkInfo.name === 'commonHelpers') {
               return 'commonHelpers.js';
